4d5e41029207bc99973ed9aad1c66638d2279bdb
Shubham26sd <shubhamdas.aj97@gmail.com>
2025-09-29 21:33:34 +0530

    Made changes in axios for backend deployment on render

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://mern-thinkboard-ckp6.vercel.app/";
+  import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://mern-thinkboard-server.onrender.com";
 
 const api = axios.create({
   baseURL: BASE_URL,
